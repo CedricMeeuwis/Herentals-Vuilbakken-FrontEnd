b25ad619638a63d58e6490731b47064f7b5e8da8
r0763618 <r0763618@student.thomasmore.be>
2021-01-19 11:28:37 +0100

    Basics

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLogin } from '../models/user-login';
+import { AuthenticateService } from '../services/authenticate.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  userLogin : UserLogin = new UserLogin('', '');
 
-  constructor() { }
+  constructor(private _authenticateService : AuthenticateService, private router: Router) { }
 
   ngOnInit(): void {
   }
+  onLogin() {
+    this._authenticateService.authenticate(this.userLogin).subscribe(result => {
+      console.log(result);
+      localStorage.setItem("token", result.token);
 
+
+      this.router.navigate(['/']);
+    }, error => {
+      console.log("Login attempt failed");
+    });
+  }
 }
