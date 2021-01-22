87706edfbff18b3ecfb77e76e5bc41ede303afab
thomasdergent <thomas.der@hotmail.com>
2021-01-22 09:56:08 +0100

    style fixes demo

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,19 +7,6 @@
 !(function($) {
   "use strict";
 
-  // Activate smooth scroll on page load with hash links in the url
-  $(document).ready(function() {
-    if (window.location.hash) {
-      var initial_nav = window.location.hash;
-      if ($(initial_nav).length) {
-        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
-      }
-    }
-  });
-
   // Mobile Navigation
   if ($('.nav-menu').length) {
     var $mobile_nav = $('.nav-menu').clone().prop({
