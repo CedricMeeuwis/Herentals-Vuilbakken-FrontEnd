f933129f6f29dd77e8b128aec23b461a74a1f7bf
Thomas Dergent <73995291+thomasdergent@users.noreply.github.com>
2021-02-16 10:01:23 +0100

    front-end styling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { EnqueteService } from '../enquete-edit/enquete.service';
 import { AuthenticateService } from '../login/services/authenticate.service';
+import { Enquete } from '../shared/models/enquete';
 
 
 @Component({
@@ -12,14 +14,31 @@ export class HeaderComponent implements OnInit {
   isMobile = false;
   overlayOn = false;
   myRole = "";
+  enquetes: Enquete[];
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private _authService: AuthenticateService
+    private _authService: AuthenticateService,
+    private _enqueteService: EnqueteService,
   ) {
     _authService.role.subscribe(val => {
       this.myRole = val;
     });
+    
+    this.getEnquetes();
+  }
+
+  getEnquetes(){
+    this._enqueteService.getActiveEnquete().subscribe(result =>{
+      this.enquetes=result;
+    });
+
+    console.log(JSON.stringify(this.enquetes));
+  }
+
+  openEnquete(id: number){
+    this.router.navigate(["/enquete-antwoord", id]);
   }
 
   logout(){
@@ -33,6 +52,8 @@ export class HeaderComponent implements OnInit {
       this.isMobile = true;
     }
     this._authService.getRole();
+
+    this.getEnquetes();
 
   }
   clickLink(){
